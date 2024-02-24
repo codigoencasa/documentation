e5b0a2224f40cf6680a4524352e1cfcce47f355e
Leifer Mendez <leifer33@gmail.com>
2024-02-24 13:14:06 +0100

    docs: more state options

--- src/components/mdx.jsx
+++ src/components/mdx.jsx
@@ -66,6 +66,14 @@ export function Row({ children }) {
   )
 }
 
+export function RowCenter({ children }) {
+  return (
+    <div className="grid grid-cols-1 items-start gap-x-16 gap-y-10 xl:grid-cols-2">
+      {children}
+    </div>
+  )
+}
+
 export function Col({ children, sticky = false }) {
   return (
     <div
