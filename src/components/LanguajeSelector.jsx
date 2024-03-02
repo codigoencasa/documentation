d4cd370dc13df069908cf5eada8b379fd1ae6282
elimeleth <juancapuanolopez@gmail.com>
2024-03-02 10:10:12 -0400

    feat: :bug: fix bug component select lang bad routing

--- src/components/LanguajeSelector.jsx
+++ src/components/LanguajeSelector.jsx
@@ -1,26 +1,31 @@
 import { usePathname } from 'next/navigation';
 import { useRouter } from 'next/router';
-import React, { useCallback, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const LanguageSelector = () => {
     const router = useRouter();
-    const [locale, setLocale] = useState(router.route.slice(0, 3).replace('/',''));
+    const [locale, setLocale] = useState(router.route.slice(0, 3));
+    const [newRoute, setNewRoute] = useState(router.route);
     const pathname = usePathname()
 
-    const handleChangeLanguage = useCallback((event) => {
+    const handleChangeLanguage = (event) => {
         const newLocale = event.target.value;
-        const path = pathname.split('/').at(2)
-
         if (newLocale !== locale) {
+            const newRoute = pathname.replace(locale, newLocale);
             setLocale(newLocale);
-            router.push(`/${newLocale}/${path}`);
+            setNewRoute(newRoute);
+            
+            router.push(`/${newRoute}`);
+            
         }
+    };
 
-    }, [locale]);
+    useEffect(() => undefined, [newRoute]);
 
     return (
         <div className="p-1 transparent">
-            <select className="p-1 bg-transparent w-[50px] rounded-md dark:text-white text-black" onChange={handleChangeLanguage} defaultValue={locale}>
+            <select className="p-1 bg-transparent w-[50px] rounded-md dark:text-white text-black"
+            onChange={handleChangeLanguage} defaultValue={locale}>
                 <option value="en">En</option>
                 <option value="pt">Pt</option>
             </select>
