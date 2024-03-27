fb5b415d7d78ff9b15f00b918a3786e7cd7b3552
Leifer Mendez <leifer33@gmail.com>
2024-03-27 17:10:35 +0100

    doc: stuff

--- services/get-profiles.js
+++ services/get-profiles.js
@@ -1,5 +1,5 @@
 
-const GH_URL = 'https://api.github.com/repos/codigoencasa/documentation/contributors'
+const GH_URL = 'https://api.github.com/users'
 
 /**
  * Retrieve all profiles
@@ -8,33 +8,31 @@ const GH_URL = 'https://api.github.com/repos/codigoencasa/documentation/contribu
 export const getProfiles = async (payload = []) => {
     payload = payload.map((c) => c.toLowerCase())
 
-    const res = await fetch(GH_URL, {
-        next: { revalidate: 3600 },
-    })
-
-    if (!res.ok) {
-        throw new Error('Failed to fetch API')
+    const fetchUser = async (username) => {
+        const res = await fetch(`${GH_URL}/${username}`,{next: { revalidate: 3600 }})
+        if (!res.ok) {
+            throw new Error(`Failed to fetch GitHub user: ${username}`)
+        }
+        return await res.json()
     }
 
-    const response = await res.json()
-    const contributors = response.map((contributor) => {
-        return {
-            id: contributor.id,
-            name: contributor.login,
-            avatar: contributor.avatar_url,
-            profile: contributor.html_url,
-            contributions: contributor.contributions,
+    const contributors = await Promise.all(payload.map(async (username) => {
+        try {
+            const user = await fetchUser(username)
+            return {
+                id: user.id,
+                name: user.login.toLowerCase(),
+                avatar: user.avatar_url,
+                profile: user.html_url,
+                contributions: user.contributions,
+            }
+        } catch (error) {
+            console.error(error.message)
+            return null
         }
-    })
+    }))
 
-    const parse = contributors.reduce((prev, current) => {
-        current.name = current.name.toLowerCase()
-        if (payload.includes(current.name)) {
-            prev.push(current)
-            return prev
-        }
-        return prev
-    }, [])
+    const parse = contributors.filter(contributor => contributor !== null)
 
     return parse
 }
